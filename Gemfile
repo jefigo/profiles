d8e4ac0b157a1fe414af8e1f8e43ac6564e5db15
Jesus Figueroa <jesus.figueroa@crowdint.com>
2013-11-27 17:18:47 -0600

    First commit

---
+++ Gemfile
@@ -0,0 +1,31 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.1'
+
+gem 'devise'
+gem 'jbuilder', '~> 1.2'
+gem 'oauth'
+gem 'omniauth', github: 'intridea/omniauth'
+gem 'omniauth-google-apps'
+gem 'pg'
+gem 'turbolinks'
+gem 'haml'
+gem 'haml-rails'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+group :assets do
+  gem 'coffee-rails', '~> 4.0.0'
+  gem 'jquery-rails'
+  gem 'sass-rails', '~> 4.0.0'
+  gem 'uglifier', '>= 1.3.0'
+end
+
+group :development, :test do
+  gem 'debugger'
+  gem 'pry'
+  gem 'rspec-rails'
+  gem 'shoulda-matchers'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Profiles::Application.config.secret_key_base = '8bcbb2a09a67e88d9f42dc8f88be217fe2a10edd339c0c2e03840a312b0eba0269bb403e85981724b78a94741a932ac50d8199143dd658408a3e36bcc081d24c'
