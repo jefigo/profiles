c2a6a6ecef3d8d69ea1810663604b922df0f0506
Jesus Figueroa <jesus.figueroa@crowdint.com>
2013-12-20 16:20:57 -0600

    Added Users controller Added Users#show

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Profiles::Application.routes.draw do
 
   root to: 'dashboard#index'
 
+  resources :users, only: :show
+
   get '/auth/:provider/callback', to: 'sessions#create'
   post '/auth/:provider/callback', to: 'sessions#create'
 
